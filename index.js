2dd6bf07dad3b484164e0ed2555d436294ef02cd
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-04-25 01:03:01 +0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+const prompt = require('prompt');
+const {createHash} = require('crypto')
+const forge = require('node-forge');
+const pki = forge.pki;
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const candidates = JSON.parse(fs.readFileSync('candidates.json', 'utf8'));
+
+
+// The machine's keypair, derived from TPS's keypair.
+const certPem = fs.readFileSync('cert/cert.pem', 'utf8');
+const cert = pki.certificateFromPem(certPem);
+const keyPem = fs.readFileSync('cert/key.pem', 'utf8');
+const key = pki.privateKeyFromPem(keyPem);
+
+prompt.start();
+var candidatesPrompt = 'Candidates : ';
+for (var i in candidates) {
+  candidatesPrompt += '\n - ' + candidates[i].name;
+}
+candidatesPrompt += '\nPlease pick by number';
+const schema = {
+  properties : {
+    cert : {
+      message : 'Cert path',
+      required : true,
+      default : 'cert/cert.pem'
+    },
+    key : {
+      message : 'Key path',
+      required : true,
+      default : 'cert/key.pem'
+    },
+    vote : {
+      message : candidatesPrompt,
+      required : true,
+      default : 2
+    },
+     
+  }
+}
+candidatesPrompt += '\nPlease vote (enter number)';
+prompt.get(schema, (err, result) => {
+
+  const vote = parseInt(result.vote);
+  if (vote < 0 || vote > candidates.length) {
+    console.log(vote + ' is not a valid candidate');
+    process.exit(1);
+  }
+
+  const voterCertPem = fs.readFileSync(result.cert, 'utf8');
+  const voterCert = pki.certificateFromPem(certPem);
+  const voterKeyPem = fs.readFileSync(result.key, 'utf8');
+  const voterKey = pki.privateKeyFromPem(keyPem);
+ 
+  /*
+  The bailout consists of :
+    - Last 8 character of sha256 hash of region
+    - Last 8 character of sha256 hash of tpsId
+    - machineId
+    - Vote
+    - Signature of previous value string (ex : a2cc25e1-b7875b4b-2-1), signed by voterMachine's key
+    - Separated by minus sign ('-')
+  */
+
+  let bailout = '';
+  bailout += createHash('sha256').update(config.region).digest('hex').substr(-8);
+  bailout += '-';
+  bailout += createHash('sha256').update(config.tpsId).digest('hex').substr(-8);
+  bailout += '-';
+  bailout += config.machineId;
+  bailout += '-';
+  bailout += vote;
+
+  var p7 = forge.pkcs7.createSignedData();
+  p7.content = forge.util.createBuffer(bailout);
+  p7.addCertificate(cert);
+  p7.addSigner({
+    key: key,
+    certificate: cert,
+    digestAlgorithm: forge.pki.oids.sha256,
+    authenticatedAttributes: [{
+      type: forge.pki.oids.contentType,
+      value: forge.pki.oids.data,
+    },
+    {
+      type: forge.pki.oids.messageDigest
+    },
+    {
+      type: forge.pki.oids.signingTime,
+      value: new Date()
+    }
+    ]
+  });
+  p7.sign();
+  var pem = forge.pkcs7.messageToPem(p7).replace(/\r?\n|\r/g, '').split('-----')[2];
+
+  bailout += '-';
+  bailout += pem
+
+  // TODO submit to DPT ledger
+
+  fs.writeFile('bailouts/bailout-' + bailout.substr(-16) + '.txt', bailout + '\n', function(err){
+    if (err) {
+      console.log('Failed to print the bailout paper : ' + err);
+      process.exit(1);
+    }
+    console.log('\nThe bailout paper has been printed out. Please take it to the vote box.');
+  });
+});
